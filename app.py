b61535f98c33f45b72a29d5c3ec98220083b9a4d
auyaguari <auyaguar@hotmail.com>
2025-06-19 20:47:09 -0500

    Cambios

--- app.py
+++ app.py
@@ -10,5 +10,9 @@ def hello():
 def suma(a, b):
     return jsonify({"resultado": a + b})
 
+@app.route('/api/multiplicar/<int:a>/<int:b>', methods=['GET'])
+def multiplicar(a, b):
+    return jsonify({"resultado": a * b})
+
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
